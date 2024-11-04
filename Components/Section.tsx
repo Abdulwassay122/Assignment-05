1e6f93d7056c5c62d8c95a58d72225cde30bb9a5
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2024-11-04 12:14:33 +0500

    1.0.1

--- Components/Section.tsx
+++ Components/Section.tsx
@@ -4,7 +4,7 @@ import pic from './img/rs-group-wrap ⏵ rs-group (1).svg'
 
 export default function Section() {
   return (
-    <section className='flex justify-between w-[1920px]'>
+    <section className='flex justify-between w-[1920px] h-[826.57px]'>
       <div className='ml-[171px] w-[902px] mr-[91p] mt-[182px]'>
         <h2 className='font-bold font-LibreBodoni text-[40px] w-[496px] h-[189px] ml-[6px]'>IMPECCABLE CRAFTSMANSHIP AND FINESSE</h2>
         <p className='font-500 font-LibreBodoni text-[30px] text-[#787054] w-[902px] h-[147px] tracking-wider'>An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.</p>
