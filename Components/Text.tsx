c3a5daf7e69e8dac70febd2bcdd45bd7cd787581
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2024-11-04 12:02:57 +0500

    1.0.0

---
+++ Components/Text.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import pic from './img/rs-group-wrap ⏵ rs-group (1).svg'
+
+export default function Text() {
+  return (
+    <div className='flex justify-between w-[1920px]'>
+      <div>      
+          <img className='' src={pic.src} alt='pic'/>
+      </div>
+      <div>
+      <img className='' src={pic.src} alt='pic'/>
+      </div>
+    </div>
+  )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
+import Navbar from "@/Components/Navbar";
+import Section from "@/Components/Section";
+import Text from "@/Components/Text";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
-    
+    <Navbar/>
+    <Section/>
+    {/* <Text/> */}
     </>
   );
 }
