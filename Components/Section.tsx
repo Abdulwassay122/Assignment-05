ba5dc7d80bac32f02cff39f38cf81c99f9ef4291
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2024-11-04 12:17:20 +0500

    1.0.2

--- Components/Section.tsx
+++ Components/Section.tsx
@@ -6,8 +6,8 @@ export default function Section() {
   return (
     <section className='flex justify-between w-[1920px] h-[826.57px]'>
       <div className='ml-[171px] w-[902px] mr-[91p] mt-[182px]'>
-        <h2 className='font-bold font-LibreBodoni text-[40px] w-[496px] h-[189px] ml-[6px]'>IMPECCABLE CRAFTSMANSHIP AND FINESSE</h2>
-        <p className='font-500 font-LibreBodoni text-[30px] text-[#787054] w-[902px] h-[147px] tracking-wider'>An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.</p>
+        <h2 className='font-bold font-LibreBodoni text-[40px] w-[496px] h-[189px] ml-[6px] mb-[28px]'>IMPECCABLE CRAFTSMANSHIP AND FINESSE</h2>
+        <p className='font-500 font-LibreBodoni text-[30px] text-[#787054] w-[902px] h-[147px] tracking-wider  mb-[50px]'>An example of intricate workmanship and detail, elegant necklaces and long and short chains form a part of our desirable collection.</p>
         <button className='text-white font-500 font-LibreBodoni text-[30px] bg-[#A29875] rounded-[10px] w-[288px] h-[58px] ml-[6px]'>Explore Now</button>
       </div>
       <div className='mr-[266px]  mt-[64px]'>
